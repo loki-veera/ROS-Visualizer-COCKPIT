3bc7917ecfcac6748a10eff527deaa544c487246
zuhakarim <zuha.karim2@gmail.com>
2020-01-01 19:10:00 +0100

    able to get roslist but problem in dropdown

--- Dashboard/bash_files/rostopic_list.sh
+++ Dashboard/bash_files/rostopic_list.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 source '/home/zuha/catkin_ws/devel/setup.bash'
-
 #Zuha Code start
 if [ $1 == 2 ]
 then
-    topiclist=$(rostopic list)
-	echo "$topiclist" 
-	
-    
+topiclist=$(rostopic list)
+IFS=' ' # space is set as delimiter
+read -ra ADDR <<< "$topiclist" # topiclist is read into an array as tokens separated by IFS
+for i in "${ADDR[@]}"; do # access each element of array
+    echo "$i"
+done
 fi  
